306b11a57af22f1d60d41accf25ed5fbee45158a
Midul-islam-Russell <midulraddy121@gmail.com>
2023-01-19 21:55:20 +0600

    Add

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
 import './App.css';
-import Home from './component/Home/Home';
-import Main from './leyout/Main'
 
 function App() {
   return (
     <div className="App">
-      <Home> </Home>
-      <Main></Main>
     </div>
   );
 }
--- src/leyout/Main.js
+++ src/leyout/Main.js
@@ -1,9 +1,11 @@
 import React from 'react';
-
+import { Outlet } from 'react-router-dom';
+import Navbar from '../component/Navbar/Navbar';
 const Main = () => {
     return (
         <div>
-            <h1>Main</h1>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
         </div>
     );
 };
